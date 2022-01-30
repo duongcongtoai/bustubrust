397266845fd37395514e39df7a09941c08513709
Duong Cong Toai <duongcongtoai.fjc@gmail.com>
2022-01-30 17:41:50 +0700

    Impl cast branch

--- src/btree.rs
+++ src/btree.rs
@@ -59,7 +59,7 @@ where
                 let keys_end = node_size as usize * size_of::<K>();
                 let (raw_keys, next) = next.split_at_mut(keys_end);
                 let keys: &mut [K] = try_cast_slice_mut(raw_keys).unwrap();
-                let children_end = (node_size + 1) as usize * size_of::<V>();
+                let children_end = (node_size + 1) as usize * size_of::<i64>();
                 let (raw_children, _) = next.split_at_mut(children_end);
                 let children: &mut [i64] = try_cast_slice_mut(raw_children).unwrap();
                 page_data = PageData::B(BranchData { keys, children });
@@ -99,7 +99,7 @@ where
         let keys_end = node_size as usize * size_of::<K>();
         let (raw_keys, next) = next.split_at_mut(keys_end);
         let keys: &mut [K] = try_cast_slice_mut(raw_keys).unwrap();
-        let children_end = (node_size + 1) as usize * size_of::<V>();
+        let children_end = (node_size + 1) as usize * size_of::<i64>();
         let (raw_children, _) = next.split_at_mut(children_end);
         let children: &mut [i64] = try_cast_slice_mut(raw_children).unwrap();
         page_data = PageData::B(BranchData { keys, children });
@@ -199,6 +199,93 @@ pub mod tests {
         println!("align of header {:?}", align_of::<PageHeader>());
         println!("size of header {:?}", size_of::<PageHeader>());
     }
+
+    #[test]
+    fn test_cast_branch() {
+        let mut some_rng: Box<dyn RngCore> = Box::new(thread_rng());
+        let node_size = 7;
+        let size = 9;
+        let next = 7;
+        let level = 10;
+        let mut some_file = tempfile().unwrap();
+        let mut fake_data: [u8; PAGE_SIZE] = [0; PAGE_SIZE];
+        let mut some_page: NodePage<KeyT, ValT> =
+            NodePage::cast_branch_from_blank(node_size, &mut fake_data[..]);
+        some_page.header.size = size;
+        some_page.header.level = level;
+        let branch_data: BranchData<_>;
+        match some_page.data {
+            PageData::B(branch) => {
+                assert_eq!(node_size as usize + 1, branch.children.len());
+                assert_eq!(node_size as usize, branch.keys.len());
+                branch_data = branch;
+            }
+            PageData::L { .. } => panic!("not expect to return leaf variant"),
+        };
+
+        let mut checked_data = Vec::new(); // clone for further check
+        for i in 0..node_size as usize {
+            let key = KeyT {
+                main: some_rng.gen(),
+                sub: some_rng.gen(),
+            };
+
+            branch_data.keys[i] = key;
+            let node_id = some_rng.gen();
+            branch_data.children[i] = node_id;
+            checked_data.push((key, node_id));
+        }
+        assert_eq!(PAGE_SIZE, some_file.write(&mut fake_data[..]).unwrap());
+        let mut new_buf: [u8; PAGE_SIZE] = [0; PAGE_SIZE];
+        some_file.seek(SeekFrom::Start(0)).unwrap();
+
+        some_file.read_exact(&mut new_buf[..]).unwrap();
+        let some_page2 = NodePage::<KeyT, ValT>::cast_generic(node_size, &mut new_buf[..]);
+        some_page2.header.size = size;
+        some_page2.header.next = next;
+        let branch_data2: BranchData<_>;
+        match some_page2.data {
+            PageData::B(branch) => {
+                assert_eq!(node_size as usize + 1, branch.children.len());
+                assert_eq!(node_size as usize, branch.keys.len());
+                branch_data2 = branch;
+            }
+            PageData::L(_) => panic!("not expect to return leaf variant"),
+        };
+        assert_eq!(false, some_page2.header.is_leaf);
+        assert_eq!(level, some_page2.header.level);
+
+        assert!(
+            checked_data
+                .iter()
+                .zip(branch_data2.keys.iter())
+                .all(|(a, b)| a.0 == *b),
+            "Keys slices are not equal"
+        );
+        assert!(
+            checked_data
+                .iter()
+                .zip(branch_data2.children.iter())
+                .all(|(a, b)| a.1 == *b),
+            "Children slices are not equal"
+        );
+    }
+    /*
+        assert.NoError(t, os.WriteFile(testFile, somePage, os.ModePerm))
+        defer os.Remove(testFile)
+        file, err := os.OpenFile(testFile, os.O_RDONLY, os.ModePerm)
+        assert.NoError(t, err)
+        newBuf := make([]byte, buff.PageSize)
+        n, err := file.Read(newBuf)
+        assert.NoError(t, err)
+        assert.Equal(t, buff.PageSize, n)
+
+        h2 := castGenericNode(10, newMockPage(newBuf))
+        assert.Equal(t, h.size, h2.size)
+        assert.Equal(t, h.keys, h2.keys)
+        assert.Equal(t, h.children, h2.children)
+        assert.False(t, h2.isLeafNode)
+    } */
 
     #[test]
     fn test_cast_leaf() {
