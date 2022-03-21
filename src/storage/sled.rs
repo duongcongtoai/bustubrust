9a434a7356ec4806b80d261dc53d417b12c58924
Duong Cong Toai <duongcongtoai.fjc@gmail.com>
2022-03-21 22:20:35 +0700

    Impl seq scan using sled storage

--- src/storage/sled.rs
+++ src/storage/sled.rs
@@ -1,18 +1,29 @@
 use crate::sql::exe::Catalog;
 use crate::sql::exe::Schema;
+use crate::sql::exe::Storage;
 use crate::sql::exe::TableMeta;
 use crate::sql::exe::Tuple;
 use crate::sql::exe::RID;
 use crate::sql::tx::Txn;
 use crate::sql::Error;
 use crate::sql::SqlResult;
+use bytemuck::try_from_bytes;
 use sled::Db;
+use sled::IVec;
+use std::array::TryFromSliceError;
+use std::convert::TryInto;
+use zerocopy::AsBytes;
 
 pub struct Sled {
     pub tree: Db,
 }
 impl From<sled::Error> for Error {
     fn from(e: sled::Error) -> Error {
+        Error::Value(e.to_string())
+    }
+}
+impl From<TryFromSliceError> for Error {
+    fn from(e: TryFromSliceError) -> Error {
         Error::Value(e.to_string())
     }
 }
@@ -23,19 +34,72 @@ impl Sled {
         Ok(Sled { tree })
     }
 
-    fn insert_tuple(&self, table: &str, tuple: Tuple, rid: RID, txn: Txn) -> SqlResult<()> {
-        Ok(())
-    }
-    fn mark_delete(&self, table: &str, rid: RID, txn: Txn) -> SqlResult<()> {
+    /* fn mark_delete(&self, table: &str, rid: RID, txn: Txn) -> SqlResult<()> {
         Ok(())
     }
     fn apply_delete(&self, table: &str, rid: RID, txn: Txn) -> SqlResult<()> {
         Ok(())
-    }
+    } */
 }
 impl From<sled::transaction::TransactionError> for Error {
     fn from(e: sled::transaction::TransactionError) -> Error {
         Error::Value(e.to_string())
+    }
+}
+impl From<&Tuple> for IVec {
+    fn from(tuple: &Tuple) -> IVec {
+        let mut ret = IVec::from(vec![0; 8 + tuple.data.len()]);
+        ret[..8].clone_from_slice(&tuple.rid.to_be_bytes());
+        ret[8..].clone_from_slice(&tuple.data);
+        ret
+    }
+}
+
+impl Storage for Sled {
+    fn insert_tuple(&self, table: &str, tuple: Tuple, txn: Txn) -> SqlResult<RID> {
+        let tuple_ref = &tuple;
+        let rid = self.tree.transaction(move |tree| {
+            let rid = tree.generate_id()?;
+            let id = rid.to_be_bytes();
+            let prefix = format!("data/{}/", table.to_string());
+
+            let key_bytes = prefix
+                .into_bytes()
+                .into_iter()
+                .chain(id.iter().copied())
+                .collect::<Vec<_>>();
+            tree.insert(IVec::from(key_bytes), tuple_ref)?;
+            tree.flush();
+            Ok(rid)
+        })?;
+        Ok(rid as RID)
+    }
+    fn mark_delete(&self, table: &str, tuple: RID, txn: Txn) -> SqlResult<()> {
+        /* self.tree.transaction(move |tree|{
+            tree.remove(key)
+
+        }) */
+        todo!()
+    }
+    fn apply_delete(&self, _: &str, _: RID, _: Txn) -> SqlResult<()> {
+        todo!()
+    }
+    fn get_tuple(&self, _: &str, _: RID, _: Txn) -> SqlResult<Tuple> {
+        todo!()
+    }
+    // Scan all table
+    fn scan(&self, table: &str, txn: Txn) -> SqlResult<Box<(dyn Iterator<Item = Tuple>)>> {
+        let prefix = format!("data/{}", table.to_string());
+        let ret: Result<Vec<Tuple>, Error> = self
+            .tree
+            .scan_prefix(prefix.as_bytes())
+            .map(move |item| {
+                let (key, value) = item.expect("scanning item");
+                let rid: RID = RID::from_be_bytes(key.as_bytes().try_into()?);
+                return Ok(Tuple::construct(rid, value.to_vec()));
+            })
+            .collect();
+        Ok(Box::new(ret?.into_iter()))
     }
 }
 
@@ -52,7 +116,7 @@ impl Catalog for Sled {
             let new_scheme = bincode::serialize(&table_meta).expect("serializing schema");
 
             tree.insert(key.as_bytes(), new_scheme)?;
-
+            tree.flush();
             Ok(())
         })?;
         Ok(table_meta)
