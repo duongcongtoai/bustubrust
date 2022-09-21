998f544b8ff41be4bb3ca923ce8b9f7b73c5c0d9
Duong Cong Toai <duongcongtoai.fjc@gmail.com>
2022-09-21 08:45:02 +0700

    More notes on other protocols found

---
+++ sql/src/mvcc/types.rs
@@ -0,0 +1,14 @@
+// a variation of 2P2Pl
+pub struct TwoVTwoPL {}
+
+// serialization snapshot isolation
+pub struct SSI {}
+
+// serial safety net
+pub struct SSN {}
+// serialization graph tester protocol
+pub struct MVSGT {}
+
+pub struct MVOCC {}
+
+pub struct MVTO {}
