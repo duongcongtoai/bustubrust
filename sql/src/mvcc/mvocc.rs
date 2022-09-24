4deef4320131f5776fd8f8b2162e31439a9dddc9
Duong Cong Toai <duongcongtoai.fjc@gmail.com>
2022-09-24 11:12:46 +0700

    More notes on read/update operation

--- sql/src/mvcc/mvocc.rs
+++ sql/src/mvcc/mvocc.rs
@@ -45,18 +45,55 @@ pub struct MVOCC {
 }
 pub struct Tx {
     begin_ts: u32,
-    state: TxState,
+    state: TxPhase,
 }
-pub enum TxState {
-    Active,
+pub enum TxPhase {
+    Processing,
     Preparing,
     Comitted,
     Aborted,
 }
 
+pub struct OCCTx {
+    read_set: Vec<Tuple>,
+    // info needed to repeat scan, maybe if i scan this again, those things must be visible again,
+    // you some weird committed items
+    scan_set: Vec<()>, // I don't know its type yet
+    // before/after updated versions
+    // deleted version
+    // inserted version
+    write_set: Vec<Tuple>,
+}
+pub trait Predicate {}
+
+pub struct IndexScanOp<P: Predicate> {
+    read_time: u64,
+    index: Vec<()>,
+    predicate: P,
+}
+
 // TODO: make this generic
 impl MVOCC {
-    fn update(&self, tx: &mut Tx, key: String, payload: Vec<u8>) -> Result<(), String> {
+    fn txn_commit(tx: &mut Tx) {}
+
+    fn txn_abort(tx: &mut Tx) {}
+
+    fn txn_processing_phase_index_scan(tx: &mut Tx) {}
+
+    /// Preparation phase. During this phase the transaction determines whether it can commit or is forced to abort. If it has to
+    /// abort, it switches its state to Aborted and continues to the next
+    /// phase. If it is ready to commit, it writes all its new versions and
+    /// information about deleted versions to a redo log record and
+    /// waits for the log record to reach non-volatile storage. The
+    /// transaction then switches its state to Committed.
+    fn txn_prepare_phase(tx: &mut Tx) {}
+    // on error must switch state to abort
+    fn txn_processing_phase_update(
+        &self,
+        tx: &mut Tx,
+        key: String,
+        payload: Vec<u8>,
+    ) -> Result<(), String> {
         let versions = self.index.get_mut(&key).unwrap();
         match find_visible_version(tx, &versions) {
             None => return Err("not found visible version".to_string()),
@@ -74,30 +111,85 @@ impl MVOCC {
     }
 }
 
-// Safety: other thread may also looking at this version chain, somehow make this function
-// threadsafe
-// the return version ts must be valid ts (not any tx_id)
-// must return the end_ts of the version at the time we found it
-// I don't know if we find a version that has a tx_id, do we abort or wait for it to set_back a
-// visible timestamp
-pub fn find_visible_version(tx: &Tx, versions: &VersionChain) -> Option<(Tuple, u64)> {
-    unimplemented!()
+pub fn register_commit_dep(tx: &Tx) {
+    struct TxDepender {
+        // how many unresolved commit dependencies it still has (this tx waits for other to commit)
+        // I can't commit until this number is 0
+        commit_dep_counter: u64,
+        abort_now: bool,
+    }
+
+    struct TxDependee {
+        // on my commit, i must decr commit_dep_counter of every txn in this set
+        // on my abort, i must set abort_now of every txn in this set
+        commit_dep_set: Vec<u64>,
+    }
 }
 
-/// TODO:
-/// Tx 75 transfer 20 from Larry to John (creating 2 new versions for Larry and John)
-/// A transaction ID stored in the End field serves as a write lock and prevents other
-/// transactions from updating the same version and it identifies
-/// which transaction has updated it. A transaction Id stored in the
-/// Begin field informs readers that the version may not yet be committed and it identifies which transaction owns the version
+/// Safety: other thread may also looking at this version chain, somehow make this function
+/// threadsafe
+/// the return version ts must be valid ts (not any tx_id)
+/// must return the end_ts of the version at the time we found it
+/// I don't know if we find a version that has a tx_id, do we abort or wait for it to set_back a
+/// visible timestamp
 ///
-/// 20, (tx75)
-unsafe fn update(tx: &Tx, last_read_ts: u64, t: Tuple, new_value: Vec<u8>) -> Option<Tuple> {
+/// Read more in PAPER:
+/// While determining the visibility of a version is straightforward in
+/// principle, it is more complicated in practice as we do not want a
+/// transaction to block (wait) during normal processing. Recall that a
+/// version’s Begin or End fields can temporarily store a transaction
+/// ID, if the version is being updated. If a reader encounters such a
+/// version, determining visibility without blocking requires checking
+/// another transaction’s state and end timestamp and potentially even
+/// restricting the serialization order of transactions.
+/// Note that the returned number can either be ts or id of a txn
+pub fn find_visible_version(
+    tx: &Tx,
+    read_ts: u64,
+    versions: &VersionChain,
+) -> Option<(Tuple, u64)> {
+    unimplemented!()
+    // begin_ts == tx_id:
+    // 1. if tx_id == my_ts && end_ts == inf => visible
+
+    // 2. tx_id == other tx_b'sid
+    // 2.a. tx_b is preparing
+    // use tx_id as begin_ts and run test_visibility(...) , if true => allow speculatively read
+    // WHAT IS SPECULATIVELY READ AND WRITE/IGNORE THO
+    // 2.b. tx_b is committed
+    // use tx_id as begin_ts and run test_visibility(...)
+    // 2.c. tx_b is aborted => ignore
+    // 2.d. tx_b is terminated or not found => re_read tx.begin, it can be finalized only if it has
+    // recently finalize the begin field of this tx
+
+    // Acquire commit dependency on other Tx:
+
+    // end_ts == tx_id:
+    // 1. if tx_id == my_ts => visible
+    // 2. txn_id = other tx_e's id and tx_e's is preparing
+    // 2.a. tx_e's end_ts > read_ts => visible
+    // 2.b. tx_e's end_ts < read_ts => speculatively ignore
+    // 3. tx_e is committed: use tx_e's end_ts and v's end_ts to check visibility
+    // 4. ts_e is aborted => visible
+    // 5. ts_e is terminated or not found => re_read end_ts again .
+}
+
+/// Suppose transaction T wants to update a version V. V is updatable
+/// only if it is the latest version, that is, it has an end timestamp equal
+/// to infinity or its End field contains the ID of a transaction TE and
+/// TE’s state is Aborted. If the state of transaction TE is Active or
+/// Preparing, V is the latest committed version but there is a later
+/// uncommitted version. This is a write-write conflict. We follow the
+/// first-writer-wins rule and force transaction T to abort.
+unsafe fn update(tx: &Tx, end_ts: u64, t: Tuple, new_value: Vec<u8>) -> Option<Tuple> {
+    // if end_ts == inf, okay
+    // if it is a txn_id => check if this txn is aborted or not found
+
     // load its ts into this tuple's end ts
     let new_ts: u64 = tx.begin_ts as u64 | 1 << 32;
     let acquired = t
         .end
-        .compare_exchange(last_read_ts, new_ts, Ordering::SeqCst, Ordering::SeqCst);
+        .compare_exchange(end_ts, new_ts, Ordering::SeqCst, Ordering::SeqCst);
     // cas fail, abort, some other tx has acquired
     if let Err(_) = acquired {
         return None;
